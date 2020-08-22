f7fdc7c24350bafb220075ecda76ea04e0e33331
mokiong <mokiong1427@gmail.com>
2020-08-22 16:54:02 +0800

    'first'

---
+++ server/src/mikro-orm.config.ts
@@ -0,0 +1,12 @@
+import { Post } from "./entities/Post";
+import { __prod__ } from "./constants";
+import { MikroORM } from "@mikro-orm/core"
+
+export default {
+   entities: [Post],
+	dbName : 'lireddit',
+   user: 'postgres',
+   password: 'michaelong',
+   type: 'postgresql',
+   debug: !__prod__
+} as Parameters<typeof MikroORM.init>[0];
