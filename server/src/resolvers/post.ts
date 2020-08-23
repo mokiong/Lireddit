f3202f631dec6b2a08669c536839b9593697d456
mokiong <mokiong1427@gmail.com>
2020-08-23 22:14:31 +0800

    added CRUD with MikroORM

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -1,19 +1,63 @@
-import { Resolver, Query, Ctx, Arg, Int } from "type-graphql";
+import { Resolver, Query, Ctx, Arg, Int, Mutation } from "type-graphql";
 import { Post } from "../entities/Post";
 import { MyContext } from "src/types";
 
 @Resolver()
 export class PostResolver {
+   //------------------QUERIES------------------//
    @Query(() => [Post])
-   posts(@Ctx() { em }: MyContext): Promise<Post[]> {
-      return em.find(Post, {});
+   async posts(@Ctx() { em }: MyContext): Promise<Post[]> {
+      return await em.find(Post, {});
    }
 
    @Query(() => Post, { nullable : true })
-   post(
-      @Arg('identifier', () => Int) id: number, 
+   async post(
+      @Arg('id') id: number, 
       @Ctx() { em }: MyContext): 
       Promise<Post | null> {
-      return em.findOne(Post, { id });
+      return await em.findOne(Post, { id });
+   }
+
+   //------------------MUTATIONS------------------//
+   @Mutation(() => Post)
+   async createPost(
+      @Arg('title') title: string, 
+      @Ctx() { em }: MyContext
+   ): Promise<Post> {
+      const post = em.create(Post, { title });
+      await em.persistAndFlush(post);
+      return post;
+   }
+
+   @Mutation(() => Post, { nullable: true })
+   async updatePost(
+      @Arg('id') id: number,
+      @Arg('title', () => String, { nullable: true }) title: string, 
+      @Ctx() { em }: MyContext
+   ): Promise<Post | null> {
+      const post = await em.findOne(Post, { id });
+      if(!post){
+         return null;
+      }
+      if(typeof title !== 'undefined'){
+         post.title = title;
+         await em.persistAndFlush(post);
+      }
+      return post;
+   }
+
+   @Mutation(() => Boolean)
+   async deletePost(
+      @Arg('id') id: number, 
+      @Ctx() { em }: MyContext
+   ): Promise<boolean> {
+      try {
+         await em.nativeDelete(Post, { id });
+         return true;
+      } catch (error) {
+         console.log(error.message);
+         return false;
+      }
+      
    }
 }
