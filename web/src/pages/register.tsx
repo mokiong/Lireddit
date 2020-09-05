59fa7a0f2da0029923456b03099fcd63d2a9f01c
mokiong <mokiong1427@gmail.com>
2020-09-06 05:19:32 +0800

    init react

---
+++ web/src/pages/register.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Formik, Form } from 'formik';
+import { FormControl, FormLabel, Input, FormErrorMessage } from '@chakra-ui/core';
+import { Wrapper } from '../components/Wrapper';
+
+interface registerProps {
+
+}
+
+const Register: React.FC<registerProps> = ({}) => {
+   return(
+      <Wrapper>
+         <Formik 
+            initialValues={{ username: "", password:"" }} onSubmit={(values) => {
+            console.log(values);
+            }}
+         >
+         {({values, handleChange}) => (
+            <Form>
+               <FormControl>
+               <FormLabel htmlFor="username">Username</FormLabel>
+                  <Input value={values.username} onChange={handleChange} id="username" placeholder="username" />
+                  {/* <FormErrorMessage>{form.errors.name}</FormErrorMessage> */}
+               </FormControl>
+            </Form>
+         )}   
+         </Formik>
+      </Wrapper>
+   );
+};
+
+export default Register;
+
